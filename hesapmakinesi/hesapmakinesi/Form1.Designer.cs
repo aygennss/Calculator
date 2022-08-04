67a39a5bd6d9d4abc8b8c90efea94340231fbd57
Aygennss <73643297+aygennss@users.noreply.github.com>
2022-08-04 21:09:45 +0300

    Add files via upload

---
+++ hesapmakinesi/hesapmakinesi/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace hesapmakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ekranlabel = new System.Windows.Forms.Label();
+            this.sayi1 = new System.Windows.Forms.Button();
+            this.sayi3 = new System.Windows.Forms.Button();
+            this.sayi2 = new System.Windows.Forms.Button();
+            this.sayi5 = new System.Windows.Forms.Button();
+            this.sayi8 = new System.Windows.Forms.Button();
+            this.sayi4 = new System.Windows.Forms.Button();
+            this.sayi9 = new System.Windows.Forms.Button();
+            this.sayi6 = new System.Windows.Forms.Button();
+            this.sayi7 = new System.Windows.Forms.Button();
+            this.sayi0 = new System.Windows.Forms.Button();
+            this.esit = new System.Windows.Forms.Button();
+            this.siltus = new System.Windows.Forms.Button();
+            this.bölme = new System.Windows.Forms.Button();
+            this.carpim = new System.Windows.Forms.Button();
+            this.eksi = new System.Windows.Forms.Button();
+            this.arti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ekranlabel
+            // 
+            this.ekranlabel.BackColor = System.Drawing.Color.White;
+            this.ekranlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ekranlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ekranlabel.Location = new System.Drawing.Point(12, 9);
+            this.ekranlabel.Name = "ekranlabel";
+            this.ekranlabel.Size = new System.Drawing.Size(345, 85);
+            this.ekranlabel.TabIndex = 0;
+            this.ekranlabel.Text = "0";
+            this.ekranlabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // sayi1
+            // 
+            this.sayi1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi1.Location = new System.Drawing.Point(12, 130);
+            this.sayi1.Name = "sayi1";
+            this.sayi1.Size = new System.Drawing.Size(75, 75);
+            this.sayi1.TabIndex = 2;
+            this.sayi1.Text = "1";
+            this.sayi1.UseVisualStyleBackColor = false;
+            this.sayi1.Click += new System.EventHandler(this.sayi1_Click);
+            // 
+            // sayi3
+            // 
+            this.sayi3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi3.Location = new System.Drawing.Point(174, 130);
+            this.sayi3.Name = "sayi3";
+            this.sayi3.Size = new System.Drawing.Size(75, 75);
+            this.sayi3.TabIndex = 3;
+            this.sayi3.Text = "3";
+            this.sayi3.UseVisualStyleBackColor = false;
+            this.sayi3.Click += new System.EventHandler(this.sayi3_Click);
+            // 
+            // sayi2
+            // 
+            this.sayi2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi2.Location = new System.Drawing.Point(93, 130);
+            this.sayi2.Name = "sayi2";
+            this.sayi2.Size = new System.Drawing.Size(75, 75);
+            this.sayi2.TabIndex = 4;
+            this.sayi2.Text = "2";
+            this.sayi2.UseVisualStyleBackColor = false;
+            this.sayi2.Click += new System.EventHandler(this.sayi2_Click);
+            // 
+            // sayi5
+            // 
+            this.sayi5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi5.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi5.Location = new System.Drawing.Point(93, 211);
+            this.sayi5.Name = "sayi5";
+            this.sayi5.Size = new System.Drawing.Size(75, 75);
+            this.sayi5.TabIndex = 7;
+            this.sayi5.Text = "5";
+            this.sayi5.UseVisualStyleBackColor = false;
+            this.sayi5.Click += new System.EventHandler(this.sayi5_Click);
+            // 
+            // sayi8
+            // 
+            this.sayi8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi8.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi8.Location = new System.Drawing.Point(93, 292);
+            this.sayi8.Name = "sayi8";
+            this.sayi8.Size = new System.Drawing.Size(75, 75);
+            this.sayi8.TabIndex = 6;
+            this.sayi8.Text = "8";
+            this.sayi8.UseVisualStyleBackColor = false;
+            this.sayi8.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // sayi4
+            // 
+            this.sayi4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi4.Location = new System.Drawing.Point(12, 208);
+            this.sayi4.Name = "sayi4";
+            this.sayi4.Size = new System.Drawing.Size(75, 75);
+            this.sayi4.TabIndex = 5;
+            this.sayi4.Text = "4";
+            this.sayi4.UseVisualStyleBackColor = false;
+            this.sayi4.Click += new System.EventHandler(this.sayi4_Click);
+            // 
+            // sayi9
+            // 
+            this.sayi9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi9.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi9.Location = new System.Drawing.Point(174, 292);
+            this.sayi9.Name = "sayi9";
+            this.sayi9.Size = new System.Drawing.Size(75, 75);
+            this.sayi9.TabIndex = 10;
+            this.sayi9.Text = "9";
+            this.sayi9.UseVisualStyleBackColor = false;
+            this.sayi9.Click += new System.EventHandler(this.sayi9_Click);
+            // 
+            // sayi6
+            // 
+            this.sayi6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi6.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi6.Location = new System.Drawing.Point(174, 211);
+            this.sayi6.Name = "sayi6";
+            this.sayi6.Size = new System.Drawing.Size(75, 75);
+            this.sayi6.TabIndex = 9;
+            this.sayi6.Text = "6";
+            this.sayi6.UseVisualStyleBackColor = false;
+            this.sayi6.Click += new System.EventHandler(this.sayi6_Click);
+            // 
+            // sayi7
+            // 
+            this.sayi7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi7.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi7.Location = new System.Drawing.Point(12, 289);
+            this.sayi7.Name = "sayi7";
+            this.sayi7.Size = new System.Drawing.Size(75, 75);
+            this.sayi7.TabIndex = 8;
+            this.sayi7.Text = "7";
+            this.sayi7.UseVisualStyleBackColor = false;
+            this.sayi7.Click += new System.EventHandler(this.sayi7_Click);
+            // 
+            // sayi0
+            // 
+            this.sayi0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.sayi0.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi0.Location = new System.Drawing.Point(93, 373);
+            this.sayi0.Name = "sayi0";
+            this.sayi0.Size = new System.Drawing.Size(75, 75);
+            this.sayi0.TabIndex = 13;
+            this.sayi0.Text = "0";
+            this.sayi0.UseVisualStyleBackColor = false;
+            this.sayi0.Click += new System.EventHandler(this.sayi0_Click);
+            // 
+            // esit
+            // 
+            this.esit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.esit.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.esit.Location = new System.Drawing.Point(174, 373);
+            this.esit.Name = "esit";
+            this.esit.Size = new System.Drawing.Size(75, 75);
+            this.esit.TabIndex = 12;
+            this.esit.Text = "=";
+            this.esit.UseVisualStyleBackColor = false;
+            this.esit.Click += new System.EventHandler(this.esit_Click);
+            // 
+            // siltus
+            // 
+            this.siltus.BackColor = System.Drawing.Color.Red;
+            this.siltus.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.siltus.ForeColor = System.Drawing.Color.White;
+            this.siltus.Location = new System.Drawing.Point(12, 370);
+            this.siltus.Name = "siltus";
+            this.siltus.Size = new System.Drawing.Size(75, 75);
+            this.siltus.TabIndex = 11;
+            this.siltus.Text = "C";
+            this.siltus.UseVisualStyleBackColor = false;
+            this.siltus.Click += new System.EventHandler(this.siltus_Click);
+            // 
+            // bölme
+            // 
+            this.bölme.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.bölme.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bölme.Location = new System.Drawing.Point(282, 373);
+            this.bölme.Name = "bölme";
+            this.bölme.Size = new System.Drawing.Size(75, 75);
+            this.bölme.TabIndex = 17;
+            this.bölme.Text = "/";
+            this.bölme.UseVisualStyleBackColor = false;
+            this.bölme.Click += new System.EventHandler(this.bölme_Click);
+            // 
+            // carpim
+            // 
+            this.carpim.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.carpim.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.carpim.Location = new System.Drawing.Point(282, 292);
+            this.carpim.Name = "carpim";
+            this.carpim.Size = new System.Drawing.Size(75, 75);
+            this.carpim.TabIndex = 16;
+            this.carpim.Text = "*";
+            this.carpim.UseVisualStyleBackColor = false;
+            this.carpim.Click += new System.EventHandler(this.carpim_Click);
+            // 
+            // eksi
+            // 
+            this.eksi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.eksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.eksi.Location = new System.Drawing.Point(282, 211);
+            this.eksi.Name = "eksi";
+            this.eksi.Size = new System.Drawing.Size(75, 75);
+            this.eksi.TabIndex = 15;
+            this.eksi.Text = "-";
+            this.eksi.UseVisualStyleBackColor = false;
+            this.eksi.Click += new System.EventHandler(this.eksi_Click);
+            // 
+            // arti
+            // 
+            this.arti.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.arti.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.arti.Location = new System.Drawing.Point(282, 130);
+            this.arti.Name = "arti";
+            this.arti.Size = new System.Drawing.Size(75, 75);
+            this.arti.TabIndex = 14;
+            this.arti.Text = "+";
+            this.arti.UseVisualStyleBackColor = false;
+            this.arti.Click += new System.EventHandler(this.arti_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 475);
+            this.Controls.Add(this.bölme);
+            this.Controls.Add(this.carpim);
+            this.Controls.Add(this.eksi);
+            this.Controls.Add(this.arti);
+            this.Controls.Add(this.sayi0);
+            this.Controls.Add(this.esit);
+            this.Controls.Add(this.siltus);
+            this.Controls.Add(this.sayi9);
+            this.Controls.Add(this.sayi6);
+            this.Controls.Add(this.sayi7);
+            this.Controls.Add(this.sayi5);
+            this.Controls.Add(this.sayi8);
+            this.Controls.Add(this.sayi4);
+            this.Controls.Add(this.sayi2);
+            this.Controls.Add(this.sayi3);
+            this.Controls.Add(this.sayi1);
+            this.Controls.Add(this.ekranlabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ekranlabel;
+        private System.Windows.Forms.Button sayi1;
+        private System.Windows.Forms.Button sayi3;
+        private System.Windows.Forms.Button sayi2;
+        private System.Windows.Forms.Button sayi5;
+        private System.Windows.Forms.Button sayi8;
+        private System.Windows.Forms.Button sayi4;
+        private System.Windows.Forms.Button sayi9;
+        private System.Windows.Forms.Button sayi6;
+        private System.Windows.Forms.Button sayi7;
+        private System.Windows.Forms.Button sayi0;
+        private System.Windows.Forms.Button esit;
+        private System.Windows.Forms.Button siltus;
+        private System.Windows.Forms.Button bölme;
+        private System.Windows.Forms.Button carpim;
+        private System.Windows.Forms.Button eksi;
+        private System.Windows.Forms.Button arti;
+    }
+}
+
---
+++ hesapmakinesi/hesapmakinesi/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapmakinesi
+{
+    public partial class Form1 : Form
+    {
+        char _islem;
+        bool _ekranTemizlenecekMi;
+        float _ilksayi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "8";
+        }
+
+        private void sayi1_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0") 
+                ekranlabel.Text = " ";
+               ekranlabel.Text += "1";
+        }
+
+        private void sayi2_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "2";
+        }
+
+        private void sayi3_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "3";
+        }
+
+        private void sayi4_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "4";
+        }
+
+        private void sayi5_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "5";
+
+        }
+
+        private void sayi6_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "6";
+        }
+
+        private void sayi7_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "7";
+        }
+
+        private void sayi9_Click(object sender, EventArgs e)
+        {
+
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "9";
+        }
+
+        private void sayi0_Click(object sender, EventArgs e)
+        {
+            if (_ekranTemizlenecekMi)
+            {
+                ekranlabel.Text = " ";
+                _ekranTemizlenecekMi = false;
+            }
+            if (ekranlabel.Text == "0")
+                ekranlabel.Text = " ";
+            ekranlabel.Text += "0";
+        }
+
+        private void arti_Click(object sender, EventArgs e)
+        {
+            _islem = '+';
+            _ekranTemizlenecekMi = true;
+            _ilksayi = Convert.ToInt32(ekranlabel.Text);
+        }
+
+        private void esit_Click(object sender, EventArgs e)
+        {
+            float ikincisayi = Convert.ToInt32(ekranlabel.Text);
+           float sonuc;
+            switch(_islem)
+            {
+                case '+' :
+                    sonuc = _ilksayi + ikincisayi;
+                    break;
+                case '-':
+                    sonuc = _ilksayi - ikincisayi;
+                    break;
+
+                case '*':
+                    sonuc = _ilksayi * ikincisayi;
+                    break;
+
+                case '/':
+                    sonuc = _ilksayi / ikincisayi;
+                    break;
+
+                default:
+                    sonuc = 0;
+                    break;
+
+
+
+
+            }
+
+            ekranlabel.Text = Convert.ToString(sonuc);
+
+           
+
+        }
+
+        private void eksi_Click(object sender, EventArgs e)
+        {
+            {
+                _islem = '-';
+                _ekranTemizlenecekMi = true;
+                _ilksayi = Convert.ToInt32(ekranlabel.Text);
+            }
+        }
+
+        private void carpim_Click(object sender, EventArgs e)
+        {
+            {
+                _islem = '*';
+                _ekranTemizlenecekMi = true;
+                _ilksayi = Convert.ToInt32(ekranlabel.Text);
+            }
+        }
+
+        private void bölme_Click(object sender, EventArgs e)
+        {
+            {
+                _islem = '/';
+                _ekranTemizlenecekMi = true;
+                _ilksayi = Convert.ToInt32(ekranlabel.Text);
+            }
+        }
+
+        private void siltus_Click(object sender, EventArgs e)
+        {
+            ekranlabel.Text = "0";
+        }
+    }
+}
